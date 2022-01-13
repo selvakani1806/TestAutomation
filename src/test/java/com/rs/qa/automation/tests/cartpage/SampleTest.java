c88cc407891dbf2bbfbf7a840e81c25b74597086
selvakani1806 <94356701+selvakani1806@users.noreply.github.com>
2022-01-13 15:37:28 +0530

    Update SampleTest.java

--- src/test/java/com/rs/qa/automation/tests/cartpage/SampleTest.java
+++ src/test/java/com/rs/qa/automation/tests/cartpage/SampleTest.java
@@ -61,7 +61,7 @@ public class SampleTest {
 		//chromePrefs.put("download.default_directory", downloadFilepath);
 		//options.setExperimentalOption("prefs", chromePrefs);
 		options.addArguments("--no-sandbox");
-		//options.addArguments("--headless"); // should be enabled for Jenkins
+		options.addArguments("--headless"); // should be enabled for Jenkins
 		options.addArguments("--disable-dev-shm-usage"); // should be enabled for Jenkins
 		options.addArguments("--window-size=1920x1080"); // should be enabled for Jenkins
 		driver = new ChromeDriver(options);
